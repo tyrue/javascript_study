5c937e8f9812c64bdea85fc03642acf2c726c709
tyrue <tyrue2@gmail.com>
2019-04-08 17:47:21 +0900

    자바스크립트 공부

--- function.js
+++ function.js
@@ -2,3 +2,55 @@ function hello()
 {
     alert("hello world");
 }
+
+function list(num) {
+    for (var i = 0; i < num; i++) {
+        document.write('<li>' + i + '</li>');
+        //document.createElement('<li>' + i + '</li>');
+    }
+}
+
+var Body = 
+{
+    setColor : function(color)
+    {
+        $('body').css('color', color);
+        // document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor : function(color)
+    {
+        $('body').css('backgroundColor', color);
+        // document.querySelector('body').style.backgroundColor = color;
+    }
+}
+
+var Links = 
+{
+    setColor : function(color)
+    {
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while(i < alist.length)
+        // {
+        //     alert[i].style.color = color;
+        //     i++;
+        // }
+        $('a').css('color', color);
+    }
+}
+
+function night_day(self) {
+    if (self.value === 'day') {
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value = 'night'
+
+        Links.setColor('blue');
+    } else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        self.value = 'day'
+
+        Links.setColor('red');
+    }
+}
